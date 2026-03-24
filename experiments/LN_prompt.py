2fbb6a72ea1680ad8dc3774c8a6850503623a707
Codevidamme <hwiyou2005@gmail.com>
2026-03-24 13:04:31 +0700

    up

--- experiments/LN_prompt.py
+++ experiments/LN_prompt.py
@@ -1,5 +1,6 @@
 import os
 import inspect
+import torch
 from torch.utils.data import DataLoader
 from torchvision import transforms
 
@@ -13,17 +14,55 @@ except ImportError:
     from pytorch_lightning.callbacks import ModelCheckpoint
 
 from src.model_LN_prompt import Model
-from src.dataset_retrieval import Sketchy
+from src.dataset_retrieval import Sketchy, SketchyEval
 from experiments.options import opts
 
+
+def resolve_num_workers(requested_workers):
+    if requested_workers <= 0:
+        return 0
+
+    try:
+        available_workers = len(os.sched_getaffinity(0))
+    except (AttributeError, OSError):
+        available_workers = os.cpu_count()
+
+    if not available_workers:
+        return requested_workers
+
+    effective_workers = min(requested_workers, available_workers)
+    if effective_workers < requested_workers:
+        print(
+            'capping dataloader workers from {} to {} based on available CPUs'.format(
+                requested_workers, effective_workers
+            )
+        )
+    return effective_workers
+
 if __name__ == '__main__':
+    if torch.cuda.is_available() and hasattr(torch, 'set_float32_matmul_precision'):
+        torch.set_float32_matmul_precision('high')
+
     dataset_transforms = Sketchy.data_transform(opts)
+    effective_workers = resolve_num_workers(opts.workers)
 
     train_dataset = Sketchy(opts, dataset_transforms, mode='train', return_orig=False)
     val_dataset = Sketchy(opts, dataset_transforms, mode='val', used_cat=train_dataset.all_categories, return_orig=False)
+    val_sketch_dataset = SketchyEval(
+        opts, dataset_transforms, mode='val', domain='sketch', used_cat=train_dataset.all_categories
+    )
+    val_photo_dataset = SketchyEval(
+        opts, dataset_transforms, mode='val', domain='photo', used_cat=train_dataset.all_categories
+    )
 
-    train_loader = DataLoader(dataset=train_dataset, batch_size=opts.batch_size, num_workers=opts.workers)
-    val_loader = DataLoader(dataset=val_dataset, batch_size=opts.batch_size, num_workers=opts.workers)
+    train_loader = DataLoader(dataset=train_dataset, batch_size=opts.batch_size, num_workers=effective_workers)
+    val_loader = DataLoader(dataset=val_dataset, batch_size=opts.batch_size, num_workers=effective_workers)
+    val_sketch_loader = DataLoader(
+        dataset=val_sketch_dataset, batch_size=opts.batch_size, num_workers=effective_workers
+    )
+    val_photo_loader = DataLoader(
+        dataset=val_photo_dataset, batch_size=opts.batch_size, num_workers=effective_workers
+    )
 
     logger = TensorBoardLogger('tb_logs', name=opts.exp_name)
 
@@ -67,6 +106,8 @@ if __name__ == '__main__':
         trainer_kwargs['gpus'] = -1
     if 'resume_from_checkpoint' in trainer_signature and ckpt_path is not None:
         trainer_kwargs['resume_from_checkpoint'] = ckpt_path
+    if 'num_sanity_val_steps' in trainer_signature:
+        trainer_kwargs['num_sanity_val_steps'] = 0
     if 'enable_progress_bar' in trainer_signature:
         trainer_kwargs['enable_progress_bar'] = False
     elif 'progress_bar_refresh_rate' in trainer_signature:
@@ -81,4 +122,4 @@ if __name__ == '__main__':
     fit_signature = inspect.signature(trainer.fit).parameters
     if 'ckpt_path' in fit_signature and ckpt_path is not None:
         fit_kwargs['ckpt_path'] = ckpt_path
-    trainer.fit(model, train_loader, val_loader, **fit_kwargs)
+    trainer.fit(model, train_loader, [val_loader, val_sketch_loader, val_photo_loader], **fit_kwargs)
--- src/model_LN_prompt.py
+++ src/model_LN_prompt.py
@@ -2,7 +2,10 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torchmetrics.functional import retrieval_average_precision
+try:
+    from torchmetrics.functional.retrieval import retrieval_average_precision
+except ImportError:
+    from torchmetrics.functional import retrieval_average_precision
 
 try:
     import lightning.pytorch as pl
@@ -29,6 +32,7 @@ class Model(pl.LightningModule):
         self.opts = opts
         self.clip, _ = clip.load('ViT-B/32', device=self.device)
         self.clip.apply(freeze_all_but_bn)
+        self.clip.train()
 
         # Prompt Engineering
         self.sk_prompt = nn.Parameter(torch.randn(self.opts.n_prompts, self.opts.prompt_dim))
@@ -41,7 +45,9 @@ class Model(pl.LightningModule):
         self.best_metric = -1e3
         self.latest_train_loss = None
         self.train_step_losses = []
-        self.validation_step_outputs = []
+        self.val_loss_outputs = []
+        self.val_sketch_outputs = []
+        self.val_photo_outputs = []
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam([
@@ -77,61 +83,81 @@ class Model(pl.LightningModule):
         self.log('train_loss', train_loss, prog_bar=False, logger=True)
         self.train_step_losses.clear()
 
-    def validation_step(self, batch, batch_idx):
-        sk_tensor, img_tensor, neg_tensor, category = batch[:4]
-        img_feat = self.forward(img_tensor, dtype='image')
-        sk_feat = self.forward(sk_tensor, dtype='sketch')
-        neg_feat = self.forward(neg_tensor, dtype='image')
+    def validation_step(self, batch, batch_idx, dataloader_idx=0):
+        if dataloader_idx == 0:
+            sk_tensor, img_tensor, neg_tensor, category = batch[:4]
+            img_feat = self.forward(img_tensor, dtype='image')
+            sk_feat = self.forward(sk_tensor, dtype='sketch')
+            neg_feat = self.forward(neg_tensor, dtype='image')
 
-        loss = self.loss_fn(sk_feat, img_feat, neg_feat)
-        self.validation_step_outputs.append({
-            'val_loss': loss.detach(),
-            'sk_feat': sk_feat.detach(),
-            'img_feat': img_feat.detach(),
-            'category': category,
-        })
-        return loss
+            loss = self.loss_fn(sk_feat, img_feat, neg_feat)
+            self.val_loss_outputs.append(loss.detach())
+            return loss
+
+        image_tensor, category, filename = batch
+        if dataloader_idx == 1:
+            feat = self.forward(image_tensor, dtype='sketch')
+            self.val_sketch_outputs.append((feat.detach(), category))
+        else:
+            feat = self.forward(image_tensor, dtype='image')
+            self.val_photo_outputs.append((feat.detach(), category))
 
     def on_validation_epoch_end(self):
         if self.trainer is not None and getattr(self.trainer, 'sanity_checking', False):
-            self.validation_step_outputs.clear()
+            self.val_loss_outputs.clear()
+            self.val_sketch_outputs.clear()
+            self.val_photo_outputs.clear()
             return
 
-        Len = len(self.validation_step_outputs)
-        if Len == 0:
+        query_len = len(self.val_sketch_outputs)
+        gallery_len = len(self.val_photo_outputs)
+        if query_len == 0 or gallery_len == 0:
+            self.val_loss_outputs.clear()
+            self.val_sketch_outputs.clear()
+            self.val_photo_outputs.clear()
             return
 
-        val_loss = torch.stack([self.validation_step_outputs[i]['val_loss'] for i in range(Len)]).mean()
-        query_feat_all = torch.cat([self.validation_step_outputs[i]['sk_feat'] for i in range(Len)])
-        gallery_feat_all = torch.cat([self.validation_step_outputs[i]['img_feat'] for i in range(Len)])
-        all_category = np.array(sum([list(self.validation_step_outputs[i]['category']) for i in range(Len)], []))
+        val_loss = None
+        if len(self.val_loss_outputs) > 0:
+            val_loss = torch.stack(self.val_loss_outputs).mean()
 
+        query_feat_all = torch.cat([self.val_sketch_outputs[i][0] for i in range(query_len)])
+        gallery_feat_all = torch.cat([self.val_photo_outputs[i][0] for i in range(gallery_len)])
+        all_sketch_category = np.array(sum([list(self.val_sketch_outputs[i][1]) for i in range(query_len)], []))
+        all_photo_category = np.array(sum([list(self.val_photo_outputs[i][1]) for i in range(gallery_len)], []))
 
-        ## mAP category-level SBIR Metrics
         gallery = gallery_feat_all
         ap = torch.zeros(len(query_feat_all))
         for idx, sk_feat in enumerate(query_feat_all):
-            category = all_category[idx]
-            distance = -1*self.distance_fn(sk_feat.unsqueeze(0), gallery)
+            category = all_sketch_category[idx]
+            similarity = F.cosine_similarity(sk_feat.unsqueeze(0), gallery)
             target = torch.zeros(len(gallery), dtype=torch.bool)
-            target[np.where(all_category == category)] = True
-            ap[idx] = retrieval_average_precision(distance.cpu(), target.cpu())
+            target[np.where(all_photo_category == category)] = True
+            ap[idx] = retrieval_average_precision(similarity.cpu(), target.cpu())
 
         mAP = torch.mean(ap)
-        self.log('val_loss', val_loss, prog_bar=False, logger=True)
+        if val_loss is not None:
+            self.log('val_loss', val_loss, prog_bar=False, logger=True)
         self.log('mAP', mAP, prog_bar=False, logger=True)
 
         if self.global_step > 0:
             self.best_metric = max(self.best_metric, mAP.item())
 
+        current_train_loss = self.latest_train_loss
+        if len(self.train_step_losses) > 0:
+            current_train_loss = torch.stack(self.train_step_losses).mean().item()
+
         epoch_summary = ['Epoch {}'.format(self.current_epoch + 1)]
-        if self.latest_train_loss is not None:
-            epoch_summary.append('train_loss: {:.4f}'.format(self.latest_train_loss))
-        epoch_summary.append('val_loss: {:.4f}'.format(val_loss.item()))
+        if current_train_loss is not None:
+            epoch_summary.append('train_loss: {:.4f}'.format(current_train_loss))
+        if val_loss is not None:
+            epoch_summary.append('val_loss: {:.4f}'.format(val_loss.item()))
         epoch_summary.append('mAP: {:.4f}'.format(mAP.item()))
         epoch_summary.append('best_mAP: {:.4f}'.format(self.best_metric))
         print(' | '.join(epoch_summary))
-        self.validation_step_outputs.clear()
+        self.val_loss_outputs.clear()
+        self.val_sketch_outputs.clear()
+        self.val_photo_outputs.clear()
 
     def on_save_checkpoint(self, checkpoint):
         checkpoint['best_metric'] = self.best_metric
